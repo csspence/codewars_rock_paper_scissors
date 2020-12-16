732683b8480c99db60065ef8ba85f922c6f494a2
Crew Spence <csspence123@gmail.com>
2020-12-16 06:42:40 -0600

    Wrote working solution.

--- codewars_rock_paper_scissors.js
+++ codewars_rock_paper_scissors.js
@@ -10,4 +10,29 @@ rps('paper','paper') // Draw!
 */
 
 const rps = (p1, p2) => {
-};
+  if(p1 === 'scissors') {
+    if(p2 === 'scissors') {
+      return 'Draw!';
+    } else if(p2 === 'rock') {
+      return 'Player 2 won!';
+    } else {
+      return 'Player 1 won!';
+    }
+  } else if(p1 === 'paper') {
+    if(p2 === 'scissors') {
+      return 'Player 2 won!';
+    } else if(p2 === 'rock') {
+      return 'Player 1 won!';
+    } else {
+      return 'Draw!';
+    }
+  } else {
+    if(p2 === 'scissors') {
+      return 'Player 1 won!';
+    } else if(p2 === 'rock') {
+      return 'Draw!';
+    } else {
+      return 'Player 2 won!';
+    }
+  }
+}
